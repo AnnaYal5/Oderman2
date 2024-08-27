527d12b532b38b107814a829843eee698471e656
Anna <gannam08@gmail.com>
2024-08-27 08:43:02 +0300

    add Oderman

--- oderman/app.py
+++ oderman/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
 pizzas = [
@@ -6,13 +6,31 @@ pizzas = [
     {'name': "Папероні", "ingredients": "соус , сир, папероні", "price": "190 грн"},
     {'name': "Гавайська", "ingredients": "соус , сир, ананас, шинка", "price": "200 грн"}
 ]
+test_name = "Discount base"
+discount = "4"
+customers = [
+    {'name': "Anna", 'order': "5"},
+    {'name': "Dany", 'order': "7"},
+    {'name': "Rita", 'order': "1"},
+    {'name': "Taya", 'order': "6"},
+]
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 @app.route('/menu')
 def menu():
-    return render_template('menu.html',pizzas=pizzas)
+    return render_template('menu.html', pizzas=pizzas)
 
-if __name__ ==  '__main__':
+@app.route('/orders')
+def orders():
+    context = {
+        "customers": customers,
+        "test_name": test_name,
+        "discount": discount
+    }
+    return render_template('orders.html', **context)
+
+if __name__ == '__main__':
     app.run(debug=True)
